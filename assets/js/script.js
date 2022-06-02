7d2272e40b846aeec0d8f13e285539a52c101d60
Santhosh Sivan A <santhoshsivan18g207@gmail.com>
2022-06-02 18:26:10 +0530

    Assignment SASS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.myNavbarToggler').onclick = () => {
+  document.querySelector('.line1').classList.toggle('anim1');
+  document.querySelector('.line2').classList.toggle('anim2');
+  document.querySelector('.line3').classList.toggle('anim3');
+  document.querySelector('.myNavigation').classList.toggle('anim4');
+}
